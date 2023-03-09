75df358c6a755d54ed2b46182197d63b4e8f936b
Mick Mednikov <the.cuprum@gmail.com>
2023-03-09 17:08:06 +0500

    Clean and optimize

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import fs from 'fs';
 import path from 'path';
-import process from 'process';
+import { cwd } from 'process';
+import { readFileSync } from 'fs';
 import _ from 'lodash';
 
-const getPath = (filePath) => path.resolve(process.cwd(), filePath);
-const getFile = (obj) => fs.readFileSync(getPath(obj), 'utf-8');
-
-const findDifference = (data1, data2) => {
+const getDiff = (data1, data2) => {
   const keys = _.sortBy(_.uniq([...Object.keys(data1), ...Object.keys(data2)]));
-  const difference = keys.reduce((acc, key) => {
+  const diff = keys.reduce((acc, key) => {
     const value1 = _.get(data1, key);
     const value2 = _.get(data2, key);
     if (value1 === value2) {
@@ -22,15 +19,16 @@ const findDifference = (data1, data2) => {
   }, [])
     .filter(([, value]) => value !== undefined)
     .map(([key, value]) => `  ${key}: ${value}`).join('\n');
-  return difference;
+
+  return diff;
 };
 
+const getPath = (file) => path.resolve(cwd(), file);
 const genDiff = (filepath1, filepath2) => {
-  const file1 = getFile(filepath1);
-  const file2 = getFile(filepath2);
-  const data1 = JSON.parse(file1.toString());
-  const data2 = JSON.parse(file2.toString());
-  const difference = findDifference(data1, data2);
+  const file1 = readFileSync(getPath(filepath1), 'utf-8');
+  const file2 = readFileSync(getPath(filepath2), 'utf-8');
+  const difference = getDiff(JSON.parse(file1), JSON.parse(file2));
+
   return difference;
 };
 
